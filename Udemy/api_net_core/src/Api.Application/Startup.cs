79985a095cc66860cd88a21c973cb5efe433f980
Rela <marciorela75@gmail.com>
2020-02-19 09:12:26 -0300

    Injeção de dependências

--- Udemy/api_net_core/src/Api.Application/Startup.cs
+++ Udemy/api_net_core/src/Api.Application/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Api.CrossCutting.DependencyInjection;
 
 namespace Application
 {
@@ -24,6 +25,7 @@ namespace Application
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            ConfigureService.ConfigureDependenciesService(services);
             services.AddControllers();
         }
 
