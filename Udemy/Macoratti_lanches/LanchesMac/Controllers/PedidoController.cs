2dfedddf2566d3e989d379803eaf44991ca65702
Rela <marciorela75@gmail.com>
2020-03-08 22:07:37 -0300

    Curso Macoratti - Lanches

---
+++ Udemy/Macoratti_lanches/LanchesMac/Controllers/PedidoController.cs
@@ -0,0 +1,64 @@
+﻿using LanchesMac.Models;
+using LanchesMac.Repositories.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesMac.Controllers
+{
+    public class PedidoController : Controller
+    {
+        private readonly IPedidoRepository _pedidoRepository;
+        private readonly CarrinhoCompra _carrinhoCompra;
+
+        public PedidoController(IPedidoRepository pedidoRepository, CarrinhoCompra carrinhoCompra)
+        {
+            _pedidoRepository = pedidoRepository;
+            _carrinhoCompra = carrinhoCompra;
+        }
+
+        [Authorize]
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Authorize]
+        public IActionResult Checkout(Pedido pedido)
+        {
+            var items = _carrinhoCompra.GetCarrinhoCompraItens();
+            
+            _carrinhoCompra.CarrinhoCompraItens = items;
+
+            if (_carrinhoCompra.CarrinhoCompraItens.Count == 0)
+            {
+                ModelState.AddModelError("", "Seu carrinho esta vazio, que tal incluir um lanche...");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _pedidoRepository.CriarPedido(pedido);
+
+                TempData["Cliente"] = pedido.Nome;
+                TempData["NumeroPedido"] = pedido.PedidoId;
+                TempData["DataPedido"] = pedido.PedidoEnviado;
+                TempData["TotalPedido"] = _carrinhoCompra.GetCarrinhoCompraTotal();
+
+                _carrinhoCompra.LimparCarrinho();
+                return RedirectToAction("CheckoutCompleto");
+            }
+
+            return View(pedido);
+        }
+
+        public IActionResult CheckoutCompleto()
+        {
+            ViewBag.Cliente = TempData["Cliente"];
+            ViewBag.DataPedido = TempData["DataPedido"];
+            ViewBag.NumeroPedido = TempData["NumeroPedido"];
+            ViewBag.TotalPedido = TempData["TotalPedido"];
+            ViewBag.CheckoutCompletoMensagem = "Obrigado pelo seu pedido :) ";
+            return View();
+        }
+    }
+}
---
+++ Udemy/Macoratti_lanches/LanchesMac/Migrations/20180107125050_atualizaPedido.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace LanchesMac.Migrations
+{
+    public partial class atualizaPedido : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "PedidoEntregueEm",
+                table: "Pedidos",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DescricaoCurta",
+                table: "Lanches",
+                maxLength: 200,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 100);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PedidoEntregueEm",
+                table: "Pedidos");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DescricaoCurta",
+                table: "Lanches",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 200);
+        }
+    }
+}
